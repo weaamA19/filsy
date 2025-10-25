b3d9587620649a21105efd53f8f75c6e6b3ac7ea
Weaam Ajoor <weaam.ajoor@gmail.com>
2025-10-25 19:44:44 +0300

    Updating Apps Script

--- api/proxy.js
+++ api/proxy.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyLV4aJ7M2z-TydeWZzvHrEJerGYHDEeZ4nqNS7xy1vFhSZ3cJrkoDG9cb-IjMRCSBykg/exec";
+  const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwzWNtFeLy0EkACPx_cS7j2dB_q_dFkjurIeE7l8tw2beQsCkQHVW7-nAKVgDLoJuGADA/exec";
 
   try {
     const response = await fetch(APPS_SCRIPT_URL, {
